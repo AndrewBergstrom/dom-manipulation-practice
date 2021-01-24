137c465f497286d377bb5de38893507b1c753ff0
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-24 09:18:41 -1000

    using createTextNode to add delete button

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var listItems = document.getElementsByTagName("li");
+var listItems = getElementsByTagName("li");
 
 function inputLength() {
 	return input.value.length;
-};
+}
 
 function createListElement() {
 	var li = document.createElement("li");
@@ -14,50 +14,100 @@ function createListElement() {
 	input.value = "";
 
 	var btn = document.createElement("button");
-btn.appendChild(document.createTextNode("Delete!"));
-li.appendChild(btn);
-btn.onclick = removeParent;
-};
-
-
+	btn.appendChild(document.createTextNode(input.value));
+}
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
-		deleteButton();
 	}
-};
+}
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
-		deleteButton();
 	}
-};
+}
 
 button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
+
+input.addEventListener("keypress", addListAfterKeypress)
 
 
-ul.onclick = function(event){
-	var target =event.target;
-	target.classList.toggle("done");
-};
 
-function listLength() {
-	return listItems.length;
-};
 
-function deleteButton(){
-	var btn = document.createElement("button");
-	btn.appendChild(document.createTextNode("Delete"));
-	listItems[i].appendChild(btn);
-	btn.onclick = removeParent;
-};
 
-for(i = 0; i < listLength(); i++){
-	deleteButton();
-};
-function removeParent(evt){
-	evt.target.parentNode.remove();
-}
+
+
+
+
+
+
+
+
+
+// var button = document.getElementById("enter");
+// var input = document.getElementById("userinput");
+// var ul = document.querySelector("ul");
+// var listItems = document.getElementsByTagName("li");
+
+// function inputLength() {
+// 	return input.value.length;
+// };
+
+// function createListElement() {
+// 	// Creating new List Item
+// 	var li = document.createElement("li");
+// 	li.appendChild(document.createTextNode(input.value));
+// 	ul.appendChild(li);
+// 	input.value = "";
+
+// 	// Creating Delete Button
+// 	var btn = document.createElement("button");
+// 	btn.appendChild(document.createTextNode("Delete!"));
+// 	li.appendChild(btn);
+// 	btn.onclick = removeParent;
+// };
+
+
+
+// function addListAfterClick() {
+// 	if (inputLength() > 0) {
+// 		createListElement();
+// 		deleteButton();
+// 	}
+// };
+
+// function addListAfterKeypress(event) {
+// 	if (inputLength() > 0 && event.keyCode === 13) {
+// 		createListElement();
+// 		deleteButton();
+// 	}
+// };
+
+// button.addEventListener("click", addListAfterClick);
+// input.addEventListener("keypress", addListAfterKeypress);
+
+
+// ul.onclick = function (event) {
+// 	var target = event.target;
+// 	target.classList.toggle("done");
+// };
+
+// function listLength() {
+// 	return listItems.length;
+// };
+
+// function deleteButton() {
+// 	var btn = document.createElement("button");
+// 	btn.appendChild(document.createTextNode("Delete"));
+// 	listItems[i].appendChild(btn);
+// 	btn.onclick = removeParent;
+// };
+
+// for (i = 0; i < listLength(); i++) {
+// 	deleteButton();
+// };
+// function removeParent(evt) {
+// 	evt.target.parentNode.remove();
+// }
